789ac1cec4987ac72d69e552dbfd9cf6d53e39dd
Liza Zayats <liza2007zayats@gmail.com>
2025-12-29 08:37:45 +0300

    sortModule.js

--- modules/sortModule.js
+++ modules/sortModule.js
@@ -1,6 +1,5 @@
 function sortStringsIgnoringSpaces(arr) {
     return [...arr].sort((a, b) => {
-        // Убираем все пробелы для сравнения
         const aClean = a.replace(/\s+/g, '');
         const bClean = b.replace(/\s+/g, '');
         return aClean.localeCompare(bClean);
