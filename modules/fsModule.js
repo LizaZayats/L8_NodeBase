2caeb889f3bda1d9935eb1fed10f97fda82b3724
Liza Zayats <liza2007zayats@gmail.com>
2025-12-29 08:36:49 +0300

    fsModule.js

--- modules/fsModule.js
+++ modules/fsModule.js
@@ -1,9 +1,7 @@
 const syncFs = require('../fs-tasks/sync');
 const asyncFs = require('../fs-tasks/async');
 
-// Реэкспортируем все функции
 module.exports = {
-    // Синхронные функции
     writeFile: syncFs.writeFile,
     readFile: syncFs.readFile,
     updateFile: syncFs.updateFile,
@@ -15,7 +13,7 @@ module.exports = {
     getAllFiles: syncFs.getAllFiles,
     cleanProject: syncFs.cleanProject,
     
-    // Асинхронные функции
+   
     writeFileAsync: asyncFs.writeFileAsync,
     readFileAsync: asyncFs.readFileAsync,
     updateFileAsync: asyncFs.updateFileAsync,
